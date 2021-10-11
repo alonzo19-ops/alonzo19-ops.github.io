913d126af5bd7794ecc3274a64feee59e067cd81
Cecilia Alonzo <alonzo.c@northeastern.edu>
2021-10-11 05:05:26 -0400

    weed strains

---
+++ Homework/10-05/sayText.js
@@ -0,0 +1,15 @@
+
+            //print value to orderTotal 
+            document.getElementById("orderTotal").value=total;
+        }console.log('Weed Measurement Guide');
+//Prints: Weed Measurement Guide
+const WeedCatalog = (name, grams, price,strain) => {
+  return {
+    name: A Gram,
+    grams: 1,
+    price: $20,
+    strain() {
+        console.log('Sativa');
+    }
+  };
+};
