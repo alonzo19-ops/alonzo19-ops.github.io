0f510a371f4377ec98c7ec4cc170278145dc485d
Cecilia Alonzo <alonzo.c@northeastern.edu>
2021-10-11 06:49:39 -0400

    y

--- Homework/10-05/sayText.js
+++ Homework/10-05/sayText.js
@@ -2,14 +2,28 @@
             //print value to orderTotal 
             document.getElementById("orderTotal").value=total;
         }console.log('Weed Measurement Guide');
+
+function myFunction() {
+    var txt;
+    var r = confirm 
+    ("Are you the age of 21 or older?"); 
+    if (r == true) { txt = "Great, proceed to website!";
+ }  else { txt = "You aren't old enough to access this site"; }
+ document.
+      getElementById
+      ("conf")
+      .innerHTML = txt;
+}
+
 //Prints: Weed Measurement Guide
 const WeedCatalog = (name, grams, price,strain) => {
-  return {
-    name: A Gram,
-    grams: 1,
-    price: $20,
-    strain() {
-        console.log('Sativa');
-    }
+    return {
+      name: A Gram,
+      grams: 1,
+      price: $20,
+      strain() {
+          console.log('Sativa');
+      
+      }
+    };
   };
-};
