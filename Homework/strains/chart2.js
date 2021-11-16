e4c71e9556a847c4d3355f94de32223aeb1f8391
Cecilia Alonzo <alonzo.c@northeastern.edu>
2021-11-16 11:04:40 -0500

    l

--- Homework/strains/chart2.js
+++ Homework/strains/chart2.js
@@ -1,4 +1,4 @@
-d3.select("#chart1")
+d3.select("#chart2")
   .on("mousemove", function() {
 
     var tooltip = d3.select("#tooltip")
@@ -6,8 +6,8 @@ d3.select("#chart1")
       .style("top", d3.event.pageY + 20 + "px")
       .style("left", d3.event.pageX + 20 + "px");
 
-    tooltip.select("#title").html("Cannabis Strains");
-    tooltip.select("#value").html("Differences between strains");
+    tooltip.select("#title").html("5 Strains Graphs");
+    tooltip.select("#value").html("graph displaying levels of thc/cbd");
 
   })
   .on("mouseout", function() {
