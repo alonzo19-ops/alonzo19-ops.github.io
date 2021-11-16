f33bed36f115864761d9ed3239da09f1743a736a
Cecilia Alonzo <alonzo.c@northeastern.edu>
2021-11-16 06:54:53 -0500

    commit

---
+++ Homework/strains/banner.js
@@ -0,0 +1,25 @@
+var url = "var url = "var url = "https://c.tenor.com/COljs_3NzNgAAAAC/420-421.gif";
+d3.json(url, function(error, data) {
+
+  if (!error) {
+    d3.select("#banner")
+      .style("background-image", "url('" + data[0].url + "')");
+  }
+
+});;
+d3.json(url, function(error, data) {
+
+  if (!error) {
+    d3.select("#banner")
+      .style("background-image", "url('" + data[0].url + "')");
+  }
+
+});://api.thecatapi.com/v1/images/search";
+d3.json(url, function(error, data) {
+
+  if (!error) {
+    d3.select("#banner")
+      .style("background-image", "url('" + data[0].url + "')");
+  }
+
+});
