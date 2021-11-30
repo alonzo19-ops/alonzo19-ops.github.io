8e1661fe0e20f9fda30bf51ca1f1706afed5a4c7
Cecilia Alonzo <alonzo.c@northeastern.edu>
2021-11-30 15:51:38 -0500

    bar graph, and drop down menue in progress

--- Homework/strains/chart1.js
+++ Homework/strains/chart1.js
@@ -1,10 +1,12 @@
 d3.select("#chart1")
 .on("mousemove", function() {
+  d3.select("#tooltip")
 
-    var tooltip = d3.select("#tooltip")
       .style("display", "block")
       .style("top", d3.event.pageY + 20 + "px")
-      .style("left", d3.event.pageX + 20 + "px");
+      .style("left", d3.event.pageX + 20 + "px")
+      .html("Bar Chart showing the makeup of different strains");
+
 
     tooltip.select("#title").html("Cannabis Strains");
     tooltip.select("#value").html("Differences between strains");
@@ -15,36 +17,4 @@ d3.select("#chart1")
       .style("display", "none");
   });
 
-  var bar_ctx = document.getElementById('chart1').getContext('2d');
-
-  var purple_pink_gradient = bar_ctx.createLinearGradient(0, 0, 0, 600);
-  purple_pink_gradient.addColorStop(0, 'pink');
-  purple_pink_gradient.addColorStop(1, 'purple');
-
-  var bar_chart = chart1 (bar_ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Wonder Woman", "Werewolf",],
-        datasets: [{
-            label: '# of Cannabinoids',
-            data: [55, 45],
-            backgroundColor: purple_pink_gradient,
-            hoverBackgroundColor: purple_pink_gradient,
-            hoverBorderWidth: 2
-        }]
-    },
-  options: {
-    scales : {
-        yAxes: [{
-          categorySpacing: 0,
-          barthickness: 20
-        }],
-        xAxes: [{
-          ticks: {
-             beginAtZero:true
-          }
-        }]
-    }
-  }
-});
-    
+ 
