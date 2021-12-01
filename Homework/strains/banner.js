cd72d59354ff5b47fb763134f2fbdfa636d68c06
Cecilia Alonzo <alonzo.c@northeastern.edu>
2021-11-30 19:11:01 -0500

    image

--- Homework/strains/banner.js
+++ Homework/strains/banner.js
@@ -1,6 +1,6 @@
 var img = document.createElement("img"); 
  
-img.src = "https://www.bing.com/images/search?view=detailV2&ccid=nA48noRr&id=058658D268B43DD0B412802A1C7071D13757A2E3&thid=OIP.nA48noRrPUqUoVYKtGwl8gHaEL&mediaurl=https%3a%2f%2fwww.wikileaf.com%2fthestash%2fwp-content%2fuploads%2f2018%2f08%2fcannabinoids.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.9c0e3c9e846b3d4a94a1560ab46c25f2%3frik%3d46JXN9FxcBwqgA%26pid%3dImgRaw%26r%3d0&exph=769&expw=1365&q=cannabis+different+strains+drawings&simid=608055704428366510&FORM=IRPRST&ck=12F72B9CBFE7B17C221EABD2B8B150C9&selectedIndex=120"; 
+img.src = "https://media.gettyimages.com/photos/cannabis-leaf-on-colored-background-picture-id1276624551?s=2048x2048"; 
 var src = document.getElementById("x"); 
  
 
