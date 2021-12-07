0b6408d48a15f1f55c7e8736101d9fc8ef849fac
Cecilia Alonzo <alonzo.c@northeastern.edu>
2021-12-07 12:17:20 -0500

    images

--- Homework/strains/chart2.js
+++ Homework/strains/chart2.js
@@ -6,8 +6,8 @@ d3.select("#chart2")
       .style("top", d3.event.pageY + 20 + "px")
       .style("left", d3.event.pageX + 20 + "px");
 
-    tooltip.select("#title").html("5 Strains Graphs");
-    tooltip.select("#value").html("graph displaying levels of thc/cbd");
+    tooltip.select("#title").html("3 Strains Graphs");
+    tooltip.select("#value").html("graph displaying Percentage of Indica and Sativa");
 
   })
   .on("mouseout", function() {
